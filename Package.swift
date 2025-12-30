b39ea7dbd54d3597cf07f5a1bd176d8e2cf14954
Angelos Staboulis <angelosstampoulisgithub@gmail.com>
2025-12-30 15:04:49 +0200

    Commit correct Package.swift

--- Package.swift
+++ Package.swift
@@ -6,20 +6,24 @@ import PackageDescription
 let package = Package(
     name: "ToastKit",
     platforms: [
-        .iOS(.v13),      // SwiftUI requires iOS 13+
-        .macOS(.v10_15), // SwiftUI requires macOS 10.15+
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .iOS(.v13),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
             name: "ToastKit",
             targets: ["ToastKit"]
-        )
+        ),
     ],
     targets: [
         .target(
-            name: "ToastKit"
-        )
+            name: "ToastKit",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ToastKitTests",
+            dependencies: ["ToastKit"],
+            path: "Tests"
+        ),
     ]
 )
